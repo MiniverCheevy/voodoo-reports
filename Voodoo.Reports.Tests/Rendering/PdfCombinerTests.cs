69f30dfd14e6bf0772d00b9809040fcb28573f06
Shawn Doucet <shawndoucet@azuresoft.net>
2017-12-28 13:45:24 -0600

    add ability to combine pdfs

---
+++ Voodoo.Reports.Tests/Rendering/PdfCombinerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Voodoo.Reports.Tests.Rendering
+{
+    [TestClass]
+    public class PdfCombinerTests : BaseTest          
+    {
+        [TestMethod]
+        public void RenderReport_ValidData_IsOk()
+        {
+            var binder = new PdfCombiner();
+            var data = base.GetData();
+            var report1 = new BordersAndShadingReport();
+            report1.Build(data);
+            binder.AddPdf(report1.Render(ReportFormat.Pdf));
+
+            var report2 = new RowSpanReport();
+            report2.Build(data);
+            binder.AddPdf(report2.Render(ReportFormat.Pdf));
+
+            var path = IoNic.PathCombineLocal(OutputPath, $"{Name}.pdf");
+            IoNic.MakeDir(OutputPath);
+            File.WriteAllBytes(path, binder.GetOutput());
+        }
+
+        public override string Name => "CombinedPdf";
+    }
+}
---
+++ Voodoo.Reports/PdfCombiner.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace Voodoo.Reports
+{
+    public class PdfCombiner
+    {
+        private PdfDocument output = new PdfDocument();
+
+        public void AddPdf(Stream stream)
+        {
+            add(stream);
+        }
+        public void AddPdf(Byte[] bytes)
+        {
+            using (var stream = new MemoryStream(bytes))
+            {
+                add(stream);
+            }
+        }
+
+        private void add(Stream stream)
+        {
+            var inputDocument = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
+
+            // Iterate pages
+            var count = inputDocument.PageCount;
+            for (var idx = 0; idx < count; idx++)
+            {
+                // Get the page from the external document...
+                PdfPage page = inputDocument.Pages[idx];
+                // ...and add it to the output document.
+                output.AddPage(page);
+            }
+        }
+
+        public byte[] GetOutput()
+        {
+            
+            using (var stream = new MemoryStream())
+            {
+                output.Save(stream);
+                return stream.ToArray();
+            }
+        }
+
+    }
+}
